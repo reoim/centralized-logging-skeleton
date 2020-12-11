decd7edb625df55109e3542ff931ea593f539fe6
EC2 Default User <ec2-user@ip-172-31-46-247.us-east-2.compute.internal>
2020-12-11 10:39:56 +0000

    initial stacks

--- bin/centralized-logging-skeleton.ts
+++ bin/centralized-logging-skeleton.ts
@@ -2,11 +2,16 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { LogBucketStack } from '../lib/log-bucket-stack';
+import { CloudtrailStack } from '../lib/cloudtrail-stack';
 import { ServerlessStack } from '../lib/serverless-stack';
 import { WebServerStack } from '../lib/webserver-stack';
-import { CloudtrailStack } from '../lib/cloudtrail-stack';
 import { LogDestinationStack } from '../lib/log-destination-stack';
 
 const envRegion = { region: 'us-east-2' };
 const app = new cdk.App();
-new LogBucketStack(app, 'LogBucketStack', { env: envRegion });
+
+// new LogBucketStack(app, 'LogBucketStack', { env: envRegion });
+new CloudtrailStack(app, 'CloudtrailStack', { env: envRegion });
+// new WebServerStack(app, 'WebServerStack', { env: envRegion });
+// new ServerlessStack(app, 'ServerlessStack', { env: envRegion });
+// new LogDestinationStack(app, 'LogDestinationStack', { env: envRegion });
--- test/centralized-logging-skeleton.test.ts
+++ test/centralized-logging-skeleton.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as CentralizedLoggingSkeleton from '../lib/centralized-logging-skeleton-stack';
+import * as LogBucket from '../lib/log-bucket-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new CentralizedLoggingSkeleton.CentralizedLoggingSkeletonStack(app, 'MyTestStack');
+    const stack = new LogBucket.LogBucketStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
