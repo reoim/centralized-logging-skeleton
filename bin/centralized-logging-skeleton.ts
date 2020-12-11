1f947c579fcf2d9c0edbc381d47f996198c52a8a
EC2 Default User <ec2-user@ip-172-31-46-247.us-east-2.compute.internal>
2020-12-11 10:54:47 +0000

    Change entry point

--- bin/centralized-logging-skeleton.ts
+++ bin/centralized-logging-skeleton.ts
@@ -10,8 +10,5 @@ import { LogDestinationStack } from '../lib/log-destination-stack';
 const envRegion = { region: 'us-east-2' };
 const app = new cdk.App();
 
-// new LogBucketStack(app, 'LogBucketStack', { env: envRegion });
-new CloudtrailStack(app, 'CloudtrailStack', { env: envRegion });
-// new WebServerStack(app, 'WebServerStack', { env: envRegion });
-// new ServerlessStack(app, 'ServerlessStack', { env: envRegion });
-// new LogDestinationStack(app, 'LogDestinationStack', { env: envRegion });
+new LogBucketStack(app, 'LogBucketStack', { env: envRegion });
+
