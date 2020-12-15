3beefee28792eeb5ac5af1935ec484fb543fe181
EC2 Default User <ec2-user@ip-172-31-46-247.us-east-2.compute.internal>
2020-12-15 08:41:56 +0000

    ecs added

--- bin/centralized-logging-skeleton.ts
+++ bin/centralized-logging-skeleton.ts
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { LogBucketStack } from '../lib/log-bucket-stack';
 
 const envRegion = { region: 'us-east-2' };
 const app = new cdk.App();
 
-new LogBucketStack(app, 'LogBucketStack', { env: envRegion });
 
